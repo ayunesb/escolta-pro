704aece35643356016c97d21d6ba6a98fc4f5268
Abraham Yunes <ayunesb@icloud.com>
2025-09-24 13:29:26 -0500

    chore(lint): pin ESLint v8 and add legacy .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,44 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: false,
+  },
+  env: {
+    es2022: true,
+    browser: true,
+    node: true,
+    jest: true,
+  },
+  plugins: ['@typescript-eslint', 'react', 'react-hooks', 'import'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  settings: {
+    react: { version: 'detect' },
+  },
+  rules: {
+    'no-case-declarations': 'warn',
+    'react/prop-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    'import/order': ['warn', { 'newlines-between': 'always' }],
+  },
+  ignorePatterns: [
+    'dist/',
+    'build/',
+    'coverage/',
+    '**/*.config.*',
+    '**/*.d.ts',
+    'supabase/functions/**/dist/**',
+  ],
+};
 const js = require("@eslint/js");
 module.exports = {
   ignorePatterns: ["dist"],
